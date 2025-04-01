19e165c84ba9080fc28241c60a4ce8666acf9df4
Yashtyagi1504 <yashtyagi1552@gmail.com>
2025-04-01 14:38:40 +0530

    submitting first time

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+let arr = ["abc","xyz","lmn","ijk"]
 const App = () => {
+  
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol>
+                {
+                  arr.map(value=>(
+                    <li>{value}</li>
+                  ))
+                }
+               </ol>
     </div>
   )
 }
